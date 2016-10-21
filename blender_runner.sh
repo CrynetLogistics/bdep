8c2e8f40be2db592689c1e79ecad144bfad9c18a
Zichen Liu <liuzc@hotmail.co.uk>
2016-10-21 09:50:53 +0100

    added jobs

--- blender_runner.sh
+++ blender_runner.sh
@@ -1,3 +1,3 @@
 PATH_TO_BLENDER=/homes/zl3114/blender/blender
 
-$PATH_TO_BLENDER -b ~/bdep/$1/job.blend -o //out/render### --engine CYCLES --render-frame 1 > render_output.log
+$PATH_TO_BLENDER -b ~/bdep/$1/job.blend -s 1 -e 5000 -o //out/render#### --engine CYCLES -a > render_output.log
